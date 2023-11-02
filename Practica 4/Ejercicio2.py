7ea4de9cf88d5768f35e9cbbb5e0e567ef5b6d6b
Alexbpx <Alexbulnespovea@gmail.com>
2023-11-02 13:05:18 +0100

    python prática 4

---
+++ Practica 4/Ejercicio2.py
@@ -0,0 +1,10 @@
+letter=input("Introduce una letra: ")#Inicializamos una variable de tipo letra
+
+if len(letter)>1 or len(letter)==0:#If que comparará el contenido de la variabke letra con la condición impuesta(que le contenido de la variable sea los de uncaracter)
+    while len(letter)>1 or len(letter)==0:#While que pedirá el contenido de la variable "letra" hasta que sea de un caracter
+        letter=input("Introduce una letra: ")
+
+if letter==letter.upper():#If que comparará el contenido final de "letra" para saber si en mayúscula o minúscula
+    print("Es una letra mayúscula")
+else:
+    print("Es una letra minúscula")
