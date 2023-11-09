96b95c6b7554107828153d3c62929249dd10094f
Alexbpx <Alexbulnespovea@gmail.com>
2023-11-09 14:42:37 +0100

    Principio

--- practica5/src/Usuario.java
+++ practica5/src/Usuario.java
@@ -1,2 +1,18 @@
 public class Usuario {
+    private String name;
+    private String second_name;
+    private Integer postal_code;
+    private String address;
+    private String email;
+    private Integer password;
+    public Usuario(String n,String s,Integer c,String a,String e,Integer p){
+        this.name=n;
+        this.second_name=s;
+        this.postal_code=c;
+        this.address=a;
+        this.email=e;
+        this.password=p;
+    }
+
+
 }
