1d7be7afaab4ff7157464bac2b1393e8401c95d1
Alexbpx <Alexbulnespovea@gmail.com>
2024-02-16 12:29:36 +0100

    16/02/2024

--- Practica11/src/Main.java
+++ Practica11/src/Main.java
@@ -1,3 +1,36 @@
-public class Main {
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MedicManagement m = new MedicManagement();
+
+        Gson gson = new Gson();
+
+        // create a reader
+        Reader reader = Files.newBufferedReader(Paths.get("Medicos.json"));
+
+        // convert JSON file to map
+        Map<String, String> map = gson.fromJson(reader, Map.class);
+
+        System.out.println(map.get("dni"));
+
+//        MedicManagement m = new Medic(
+//                map.get("dni"),
+//                map.get("name"),
+//                Integer.parseInt(map.get("age")),
+//                map.get("gender"),
+//                Double.parseDouble(map.get("salary"))
+//        );
+
+//        System.out.println(m.getName());
+
+        // close reader
+        reader.close();
+
+    }
 }
--- Practica11/src/MedicManagement.java
+++ Practica11/src/MedicManagement.java
@@ -1,21 +1,23 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Medico {
+public class MedicManagement {
 
     private FileReader fr;
     private BufferedReader br;
 
-    public Medico () throws FileNotFoundException {
+    public MedicManagement() {
         try {
             this.fr = new FileReader("pacientes.txt");
             this.br = new BufferedReader(this.fr);
             String line;
             while ((line = this.br.readLine()) != null) {
+                String[] datos = line.split(";");
+                System.out.println(datos[0]);
+
             }
-        } catch (IOException | NumberFormatException e) {
+        } catch (IOException e) {
             System.err.println("Error al leer el archivo de texto: " + e.getMessage());
         }
     }
