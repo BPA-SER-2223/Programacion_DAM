7fc456bea5babbb726f6f13414737bd05a6b8767
Alexbpx <Alexbulnespovea@gmail.com>
2024-04-23 00:28:37 +0200

    23/04/2024

--- hola/src/Main.java
+++ hola/src/Main.java
@@ -6,18 +6,19 @@ public class Main {
     public static void main(String[] args) {
         String URL = "jdbc:mysql://localhost:3306";
         String USER = "root";
-        String PASS = "1234";
+        String PASS = "2005Alex";
         Connection conn = null;
         Statement stmt = null;
         int selection = 0;
         Scanner a = new Scanner(System.in);
+
+
+
+
         try{
             conn = DriverManager.getConnection(URL, USER, PASS);
             stmt = conn.createStatement();
-            String actualizaciobn = "INSERT INTO red_social.usuarios VALUES(20, 'Alejandro', 'Bulnes Povea', '6578lexbpx', '$2a$10$OH9B70MPW2AxnKbkjwbkyug347GxAVNUrPRuPMZnSyE.TT3D3KKAW', '2005-04-06', '626330591', NULL, '2024-04-10 09:52:28', NULL)";
-            int nactualizacion = -1;
-            //nactualizacion = stmt.executeUpdate(actualizaciobn);
-            System.out.println("Se han actualizado " + nactualizacion);
+
             ResultSet rs = stmt.executeQuery("SELECT * FROM red_social.usuarios");
 
             System.out.println("Resultados de la base de datos:");
@@ -37,8 +38,6 @@ public class Main {
             int numerber_fields = 0;
             String nombre_tabla;
             String fields;
-            int num = 0;
-            int count = 0;
 
 
             do{
@@ -50,15 +49,14 @@ public class Main {
                         String table = "CREATE TABLE ";
 
                         System.out.println("¿Cuál es el nombre de la table? ");
-                        nombre_tabla = a.next();
+                        nombre_tabla = a.nextLine();
                         table = table + nombre_tabla + " (";
-                        System.out.println("¿Con cuantos campos contará la tabla?");
+                        System.out.println("¿Cuantos campos cuesta la tabla?");
                         numerber_fields  = a.nextInt();
 
                         ArrayList<String> list_fields = new ArrayList<>();
 
                         for(int i=0;i<numerber_fields;i++){
-                            count++;
                             System.out.print((i+1)+"º campo: ");
                             fields = a.next();
                             list_fields.add(fields);
@@ -69,40 +67,35 @@ public class Main {
                             selection = a.nextInt();
 
                             if(selection == 1){
-                                table = table + "VARCHAR";
+                                table = table + "VARCHAR ";
                             }
                             if(selection == 2){
-                                table = table + "INT";
+                                table = table + "INT ";
                             }
                             if(selection == 3){
-                                table = table + "CHAR";
+                                table = table + "CHAR ";
                             }
                             if(selection == 4){
-                                table = table + "DATE";
+                                table = table + "DATE ";
                             }
                             if(selection == 5){
-                                table = table + "TIME";
+                                table = table + "TIME ";
                             }
                             if(selection == 6){
-                                table = table + "DATETIME";
+                                table = table + "DATETIME ";
                             }
 
                             System.out.print("Número de caracteres: ");
-                            num = a.nextInt();
-                            table = table + "(" +(num+")");
-
-                            if(count < numerber_fields){
-                                table = table + ",";
-                            }
                         }
 
-                        table = table + ");";
-
-                        System.out.println("-----------------------------\n\t\t¡Tabla creada!\n-----------------------------\n");
-
                         stmt.executeUpdate("use red_social;");
-                        stmt.executeUpdate(table);
-
+                        //stmt.executeUpdate(
+                        //         "CREATE TABLE Prueba (" +
+                        //        "Id INT PRIMARY KEY," +
+                        //         "Puertas INT NOT NULL," +
+                        //         "color varchar(50) NOT NULL," +
+                        //         "FOREIGN KEY (Id) REFERENCES CASA(Id)" +
+                        //         ");");
                 }
             }while(selection != 0);
 
